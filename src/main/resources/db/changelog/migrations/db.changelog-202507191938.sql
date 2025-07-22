8ff116ed89e0e43bc928a74c2317f309898765df
Arth1Droid <barbosarthur7@gmail.com>
2025-07-22 20:07:25 -0300

    fix dbChangelog

--- src/main/resources/db/changelog/migrations/db.changelog-202507191938.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202507191938.sql
@@ -1,11 +1,10 @@
 --liquibase formatted sql
---changeset ArthDroid: 202507191938
--- comment: boards table create
+--changeset ArthDroid:202507191938
+--comment: Criação da tabela BOARDS
 
-CREATE TABLE BOARDS{
+CREATE TABLE BOARDS (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
 
-} ENGINE = InnoDB;
-
---rollback DROP TABLE BOARDS
+--rollback DROP TABLE BOARDS;
